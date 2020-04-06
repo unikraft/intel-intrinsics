9e2585e9c6163acd841b94fc23dfa6023e827cd7
Vlad-Andrei Badoiu <vlad_andrei.badoiu@upb.ro>
2020-04-06 22:24:05 +0300

    Add x86intrin.h
    This x86intrin.h does not include all the
    expected headers since we don't have them yet.
    
    Signed-off-by: Vlad-Andrei Badoiu <vlad_andrei.badoiu@upb.ro>
    Reviewed-by: Felipe Huici <felipe.huici@neclab.eu>

---
+++ include/x86intrin.h
@@ -0,0 +1,9 @@
+#ifndef _X86INTRIN_H_INCLUDED
+#define _X86INTRIN_H_INCLUDED
+
+/* For including AVX instructions */
+#include <immintrin.h>
+
+#include <popcntintrin.h>
+
+#endif /* _X86INTRIN_H_INCLUDED */
